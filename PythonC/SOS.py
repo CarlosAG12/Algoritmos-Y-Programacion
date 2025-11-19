bd29e2e22e6a5a300494ee3d73e824ebf3575dd0
CarlosAG12 <carlosacostagil65@gmail.com>
2025-11-18 22:18:21 -0500

    Delete PythonC directory

--- PythonC/SOS.py
+++
@@ -1,8 +0,0 @@
-caracter = input("Ingrese un caracter: ").lower()
-
-if caracter == 'a':
-    print("Android")
-elif caracter == 'i':
-    print("IOS")
-else:
-    print("Opcion invalida")
--- PythonC/rango.py
+++
@@ -1,7 +0,0 @@
-num = float(input("Ingrese el numero: "))
-
-if 3.5 < num and num <= 7.8:
-    print("Si se encuentra dentro del rango")
-
-else:
-    print("El numero no esta dentro")
